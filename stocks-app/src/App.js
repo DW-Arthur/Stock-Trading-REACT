3438ee09276e8765bd8acb88548377f8c57675f4
DW-Arthur <Wangshujian1999@gmail.com>
2021-05-26 17:16:36 -0400

    Added backend

--- stocks-app/src/App.js
+++ stocks-app/src/App.js
@@ -1,15 +1,33 @@
-import logo from './logo.svg';
 import stock from './stock.gif';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+      fetch("http://localhost:9000/testAPI")
+          .then(res => res.text())
+          .then(res => this.setState({ apiResponse: res }))
+          .catch(err => err);
+  }
+
+  componentWillMount() {
+      this.callAPI();
+  }
+
+  render() {
+    return(
+      <div className="App">
       <header className="App-header">
         <img src={stock} className="App-logo" alt="logo" />
         <p>
           Stock Trading Competition
         </p>
+        
         <a
           className="App-link"
           href="https://www.investopedia.com/"
@@ -19,10 +37,13 @@ function App() {
           Basic Trading Termninology
         </a>
         <p>User 1 <span class="tab"></span> User 2 <span class="tab"></span> User 3</p>
+        <p className="App-intro">;{this.state.apiResponse}</p>
         <noscript>Test</noscript>
       </header>
     </div>
-  );
+    )
+    
+  }
 }
 
 export default App;
