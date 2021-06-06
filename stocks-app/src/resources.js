676d18641d9a3c9e3fe229d313d6b198f43cb85c
bruce <bruceliu1107@gmail.com>
2021-06-06 17:05:25 -0400

    deleted ssss

--- stocks-app/src/resources.js
+++ stocks-app/src/resources.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Resource() {
     return(
         <div> 
-            <h1>Thissssssss leads to the investopedia webpage and a test</h1>
+            <h1>Thisssaa leads to the investopedia webpage and a test</h1>
         </div>
     )
 }
