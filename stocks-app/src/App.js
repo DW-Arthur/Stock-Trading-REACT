32223262ac44d7b4d51ec2b3dd9987c3162bf471
DESKTOP-I1B98FM\YouHengZhou <youhengzhou@cmail.carleton.ca>
2022-01-05 23:36:06 -0500

    refactor formatter using prettier

--- stocks-app/src/App.js
+++ stocks-app/src/App.js
@@ -1,60 +1,57 @@
-import './App.css';
-import React, { Component } from 'react';
-import Trade from './components/Trade'
-import {Switch, Route, Link,BrowserRouter as Router} from 'react-router-dom';
-import Home from './components/Home';
-import Reviews from './components/reviews';
-import CreateReview from './components/CreatReviews';
-import About from './components/About';
-import Links from './components/Links';
-import Shop from './components/Shop';
-import Navbar from "./components/Navbar"
+import "./App.css";
+import React, { Component } from "react";
+import Trade from "./components/Trade";
+import { Switch, Route, Link, BrowserRouter as Router } from "react-router-dom";
+import Home from "./components/Home";
+import Reviews from "./components/reviews";
+import CreateReview from "./components/CreatReviews";
+import About from "./components/About";
+import Links from "./components/Links";
+import Shop from "./components/Shop";
+import Navbar from "./components/Navbar";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { apiResponse: ""}
+    this.state = { apiResponse: "" };
   }
 
-  callAPI() {  //the object that .then returns, got passed to the next .then as parameter.
-      fetch("http://localhost:9000/testAPI") //"testAPI" invoke the function in api/routes/testAPI.js
-          .then(res => res.text()) //converte res to text format
-          .then(res => this.setState({ apiResponse: res})) //set state to store res in the apiResponse
-          .catch(err => err);
+  callAPI() {
+    //the object that .then returns, got passed to the next .then as parameter.
+    fetch("http://localhost:9000/testAPI") //"testAPI" invoke the function in api/routes/testAPI.js
+      .then((res) => res.text()) //converte res to text format
+      .then((res) => this.setState({ apiResponse: res })) //set state to store res in the apiResponse
+      .catch((err) => err);
   }
 
   componentWillMount() {
-      console.log('component will mount')
-      this.callAPI(); //componentWillMount will trgger before the initial render(when the component load)
+    console.log("component will mount");
+    this.callAPI(); //componentWillMount will trgger before the initial render(when the component load)
   }
 
   render() {
-    return(
+    return (
       <div className="App">
-          
-        <Router >
-        <Navbar></Navbar>
-          <Route path = "/" exact>
+        <Router>
+          <Navbar></Navbar>
+          <Route path="/" exact>
             <Home></Home>
           </Route>
-          <Route path='/reviews'>
+          <Route path="/reviews">
             <Reviews />
           </Route>
-          <Route path='/create'>
+          <Route path="/create">
             <CreateReview />
           </Route>
           <Route path="/about" component={About} />
           <Route path="/links" component={Links} />
           <Route path="/shop" component={Shop} />
-          <Route exact path="/trade" component ={Trade}/>
+          <Route exact path="/trade" component={Trade} />
         </Router>
         <header className="App-header">
-       {/* <p className="App-intro">;{this.state.apiResponse}</p> */}
-        {/* create the Resource page */}
-        
-        
-        
+          {/* <p className="App-intro">;{this.state.apiResponse}</p> */}
+          {/* create the Resource page */}
 
-        {/* <main>
+          {/* <main>
           <Switch>
             <Route path="/" component={Home} exact />
             <Route path="/about" component={About} />
@@ -74,8 +71,7 @@ class App extends Component {
           <Link to="/links">Links </Link>
         </div> */}
 
-        
-        {/* <a
+          {/* <a
           className="App-link"
           href="https://www.investopedia.com/"
           target="_blank"
@@ -84,17 +80,9 @@ class App extends Component {
           Basic Trading Termninology
 
         </a> */}
-        
-        
-        
-        
-      </header>
-      
-
-      
-    </div>
-    )
-    
+        </header>
+      </div>
+    );
   }
 }
 
